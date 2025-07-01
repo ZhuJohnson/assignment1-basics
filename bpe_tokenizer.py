8d2b8ff74e72226029fa805dc34bbc36b7131ae2
Johnson Zhu <ustc.johnson@gmail.com>
2025-07-01 15:08:17 +0800

    update the script for bpe tokenizer training and corresponding pass log

--- bpe_tokenizer.py
+++ bpe_tokenizer.py
@@ -83,13 +83,13 @@ def sj_bpe(input_path, start, end, special_token):
 
 #multiprocessing version of pretokenization
 def get_token_counts_mp(input_path, num_process, special_token) -> Counter:
-    num_file_splits = 12
+    num_file_splits = 1
     with open(input_path, "rb") as f:
         boundaries = find_chunk_boundaries(f, num_file_splits, special_token.encode("utf-8"))
         
     logging.info(f"Chunk boundaries: {boundaries}")
     logging.info(f"start multiprocessing with {num_process} processes")
-    res = Parallel(n_jobs=num_process, verbose=10)(
+    res = Parallel(n_jobs=1, verbose=10)(
     delayed(sj_bpe)(input_path, boundaries[i], boundaries[i+1],
                     special_token)for i in range(len(boundaries)-1))
     #the aggregated counter for token
@@ -99,79 +99,61 @@ def get_token_counts_mp(input_path, num_process, special_token) -> Counter:
     logging.info(f"Total tokens found: {len(total_res)}")
     return total_res
 
-def merge(token_dict: dict, best_pair, new_id) -> dict:
-    res = {}
+
+def merge(token_dict: dict, best_pair, new_id, vocab):
+    res, pair_offset = {}, defaultdict(int)
+    
     for token in token_dict.keys():
-        i = 0
-        new_token = []
-        while i < len(token):
-            if token[i] == best_pair[0][0] and i < len(token) - 1 and token[i+1] == best_pair[1][0]:
-                new_token.append(new_id)
-                i += 2
-            else:
-                new_token.append(token[i])
-                i += 1
-        res[tuple(new_token)] = token_dict[token]
-    return res
+        if best_pair[0] in token and best_pair[1] in token:
+            i = 0
+            new_token = []
+            while i < len(token):
+                if token[i] == best_pair[0] and i < len(token) - 1 and token[i+1] == best_pair[1]:
+                    new_token.append(new_id)
+                    if i > 0:
+                        pair_offset[(token[i-1], token[i])] -= token_dict[token]
+                        pair_offset[(token[i-1], new_id)] += token_dict[token]
+                    if i < len(token) - 2:
+                        pair_offset[(token[i+1], token[i+2])] -= token_dict[token]
+                        pair_offset[(new_id, token[i+2])] += token_dict[token]
+                    i += 2
+                else:
+                    new_token.append(token[i])
+                    i += 1
+            res[tuple(new_token)] = token_dict[token]
+        else:
+            res[tuple(token)] = token_dict[token]
+    return res, pair_offset
 
-def mp_merge(token_dict: dict, best_pair, new_id, num_process) -> Counter:
-    """
-    Merges the best pair of tokens in the token_dict and returns the updated dictionary.
-    """
-    res = Counter()
-    token_item = list(token_dict.items())
-    k, m = divmod(len(token_item), num_process)
-    res_all = Parallel(n_jobs=num_process)(
-        delayed(merge)(dict(token_item[i*k + min(i, m):(i+1)*k + min(i+1, m)]),
-                        best_pair, new_id)
-        for i in range(num_process)
-    )
-    for item in res_all:
-        res.update(item)
-    return res
-
-
-def get_max_pair(vocab, cand_pairs):
-    res = cand_pairs[0]
-    for i in range(1,len(cand_pairs)):
-        #print (vocab[res[0][0]], vocab[cand_pairs[i][0][0]])
-        if vocab[res[0][0]] < vocab[cand_pairs[i][0][0]]:
-            res = cand_pairs[i]
-        elif vocab[res[0][0]] == vocab[cand_pairs[i][0][0]]:
-            if vocab[res[1][0]] < vocab[cand_pairs[i][1][0]]:
-                res = cand_pairs[i]
-    return res
-
+#pair: (int, int)
 def sj_freq_summary(token_dict):
     pairs = Counter()
     for token, count in token_dict.items():
         if len(token) == 1: 
             continue  # Skip single-character tokens
         for i in range(len(token) - 1):
-            pair = (token[i:i+1], token[i+1:i+2])
-            pairs[tuple(pair)] += count
+            pair = (token[i], token[i+1])
+            pairs[pair] += count
     return pairs
 
 def bpe_train(token_counts, vocab_size, special_tokens, initial_vocab):
     vocab = initial_vocab.copy()
     merges = []
-    '''
-    token_dict = {}
-    for key, val in token_counts.items():
-        token_dict[tuple(key)] = val
-    '''
+
+    #t1,t2,t3 = 0,0,0
+
+    pairs, pairs_offset = Counter(), Counter()
+    pairs = sj_freq_summary(token_counts)
+
     # Merge tokens until the vocabulary size is reached
     while len(vocab) < vocab_size:
-        pairs = Counter()
-        token_item = list(token_counts.items())
-        k, m = divmod(len(token_item), 4)
-        res_all = Parallel(n_jobs=4)(
-        delayed(sj_freq_summary)(dict(token_item[i*k + min(i, m):(i+1)*k + min(i+1, m)]))
-        for i in range(4)
-        )
-        for item in res_all:
-            pairs.update(item)
+        
+        time1 = time.time()
+        pairs += pairs_offset
+        pairs_offset = Counter()
+        
         pair_heap = []
+        #time2 = time.time()
         for pair, count in pairs.items():
             heapq.heappush(pair_heap, (-count, pair))
         cand_pairs = []
@@ -179,53 +161,37 @@ def bpe_train(token_counts, vocab_size, special_tokens, initial_vocab):
         cand_pairs.append(first_pair)
         while pair_heap[0][0] == pair_freq:
             cand_pairs.append(heapq.heappop(pair_heap)[1])
-        '''
-        pairs = defaultdict(int)
-        # Count pairs of adjacent tokens
-        for token, count in token_dict.items():
-            if len(token) == 1: 
-                continue  # Skip single-character tokens
-            for i in range(len(token) - 1):
-                pair = (token[i:i+1], token[i+1:i+2])
-                pairs[tuple(pair)] += count
-        if not pairs:
-            break
-        # Find the most common pair, best_pair: [bytes,bytes]
-        #best_pair = max(pairs, key=pairs.get)
-        max_val = max(pairs.values())
-        cand_pairs = [pair for pair, count in pairs.items() if count == max_val]
-        '''
+
         if len(cand_pairs) == 1:
             best_pair = cand_pairs[0]
         else:
-            #best_pair = max(cand_pairs,key=my_comp(vocab))
-            best_pair = get_max_pair(vocab, cand_pairs)
+            best_pair = max(cand_pairs, key=lambda x: (vocab[x[0]], vocab[x[1]]))
         if pairs[best_pair] < 2:
             break  # Stop if no pairs are frequent enough
-        # Merge the best pair
 
-
-
-        #new_token = vocab[best_pair[0][0]] + vocab[best_pair[1][0]]
-        new_token = bytes((vocab[best_pair[0][0]]+vocab[best_pair[1][0]]))
+        new_token = bytes((vocab[best_pair[0]]+vocab[best_pair[1]]))
         # Add the new token to the vocabulary
         if new_token not in vocab.values():
             new_token_id = len(vocab)
         vocab[new_token_id] = new_token
-        #print (">>>", new_token, new_token_id,len(vocab))
+        
         try:
-            logging.info(f"Merging {vocab[best_pair[0][0]]} and {vocab[best_pair[1][0]]} into {new_token.decode('utf-8')} with ID {new_token_id}")
+            logging.info(f"Merging {vocab[best_pair[0]]} and {vocab[best_pair[1]]}  with ID {new_token_id}")
         except UnicodeDecodeError:
-            print (best_pair[0][0], best_pair[1][0], new_token, new_token_id)
-        #print ("-------------------", len(vocab), vocab_size)
+            print (best_pair[0], best_pair[1], new_token, new_token_id)
+
         
-        merges.append((vocab[best_pair[0][0]], vocab[best_pair[1][0]]))
-        
+        merges.append((vocab[best_pair[0]], vocab[best_pair[1]]))
+        #time3 = time.time()
         
         # Update token counts
-        #token_dict = merge(token_dict, best_pair, new_token_id)
-        # Use multiprocessing to merge tokens
-        token_counts = mp_merge(token_counts, best_pair, new_token_id, num_process=4)
+        token_counts, pairs_offset = merge(token_counts, best_pair, new_token_id, vocab)
+        pairs_offset[best_pair] = -pairs[best_pair]
+        #time4 = time.time()
+        #t1 += time2 - time1
+        #t2 += time3 - time2
+        #t3 += time4 - time3
+    #print ("!!!!", t1, t2, t3)
     return vocab, merges
 
 
@@ -270,9 +236,12 @@ def train_bpe_tokenizer(
             logging.info(f"Adding special token '{token}' with ID {new_id}")
 
     #step1. pretokenize each chunk(with multiprocessing)
-    logging.info("Starting pretokenization...")
+    time1 = time.time()
+    #logging.info("Starting pretokenization...")
     total_res = get_token_counts_mp(input_path, num_processes, special_tokens[0])
-    logging.info("Pretokenization complete.")
-
+    #logging.info("Pretokenization complete.")
+    time2 = time.time()
     vocab, merge = bpe_train(total_res, vocab_size, special_tokens, vocab)
+    time3 = time.time()
+    print (time2 - time1, time3 - time2, time3-time1)
     return vocab, merge
