75755102fc44010a78f1c6c4c462a5b15a4ed1ff
Johnson Zhu <ustc.johnson@gmail.com>
2025-07-16 16:35:25 +0800

    update rope implementation

--- transformer_utils.py
+++ transformer_utils.py
@@ -104,3 +104,66 @@ class SwiGLULayer(torch.nn.Module):
     
     def forward(self, x):
         return (silu(x @ self.w1.T) * (x @ self.w3.T)) @ self.w2.T
+
+
+class RotaryPositionalEmbedding(torch.nn.Module):
+    def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
+        """ Construct the RoPE module and create buffers if needed
+
+        Args:
+            theta (float): Θ value for the RoPE
+            d_k (int): dimension of query and key vectors
+            max_seq_len (int): Maximum sequence length that will be inputted
+            device (None, optional): Device to store the buffer on. Defaults to None.
+        """
+        super().__init__()
+        self._d_k = d_k
+        self._theta = theta
+        self._max_seq_len = max_seq_len
+        self._device = torch.device("cpu") if device is None else device
+        self.update_cache(max_seq_len)
+
+    def update_cache(self, len: int):
+        """when the sequence length is longer than max_seq_len which specified in this class,
+        dynamically update the rotation matrix
+
+        Args:
+            len (int): the current sequence length
+        """
+        self._max_seq_len = len
+        pos_multiplier = torch.arange(len, device=self._device).float()
+        theta_list =  torch.pow(self._theta, -1 * torch.arange(0, self._d_k, 2)/self._d_k).float()
+        #theta_{i,k} = i / (THETA ** (2k / d))
+        #freq is theta_{i,k}, i between 0 and max_seq_len, k between 0, d_k/2
+        freq = torch.einsum("i, j -> ij", pos_multiplier, theta_list)
+        #shape of emb: [max_seq_len, d_k]
+        #emb = torch.cat((freq,freq), dim=-1)
+        emb = freq.repeat_interleave(2, dim=-1)
+        self.register_buffer("sin", emb.sin(), persistent=False)
+        self.register_buffer("cos", emb.cos(), persistent=False)
+
+    def half_rotate(self, x):
+        #(1,2,3,4) --> (-2,1,-4,3)
+        x1 = x[..., 1::2] 
+        x2 = x[..., ::2]   
+        rotated = torch.stack((-x1, x2), dim=-1) 
+        return rotated.flatten(start_dim=-2)      
+    
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        """Process an input tensor of shape (..., seq_len, d_k) and return a tensor of the same shape.
+
+        Args:
+            x (torch.Tensor): input tensor
+            token_positions (torch.Tensor): position tensor
+
+        Returns:
+            torch.Tensor: tensor with rope 
+        """
+        max_len = torch.max(token_positions)
+        if max_len >= self._max_seq_len:
+            self.update_cache(max_len + 1)
+            self._max_seq_len = max_len
+        return x * self.cos[token_positions] + self.half_rotate(x) * self.sin[token_positions]
+        
+        
